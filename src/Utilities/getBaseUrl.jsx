27f666bf24371f4b1599aaeef43bfbd89a750ac8
Britney Nunes <britneynunes8@gmail.com>
2025-11-18 10:28:39 +0200

    Changing to elastic IP

--- src/Utilities/getBaseUrl.jsx
+++ src/Utilities/getBaseUrl.jsx
@@ -2,8 +2,8 @@ export function getBaseUrl() {
   // Get the current URL from the browser
   const url = new URL(window.location.href);
 
-  // Extract the 'ip' query parameter from the URL, default to '44.209.222.79' if not present
-  const ip = url.searchParams.get("ip") || '44.209.222.79';
+  // Extract the 'ip' query parameter from the URL, default to '98.91.62.10' if not present
+  const ip = url.searchParams.get("ip") || '98.91.62.10';
 
   // Log the IP address for debugging purposes
   console.log(`Using IP address: ${ip}`);
