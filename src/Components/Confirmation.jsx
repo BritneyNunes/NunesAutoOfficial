f4dfc0ac739ff665d3533fbba91b0a8dcf5628b1
Britney Nunes <britneynunes8@gmail.com>
2025-11-26 12:48:09 +0200

    Commenting out email

--- src/Components/Confirmation.jsx
+++ src/Components/Confirmation.jsx
@@ -16,9 +16,9 @@ function Confirmation() {
           Your order has been received and is being prepared with care.
         </p>
 
-        <p className="thankyou-blessing">
+        {/* <p className="thankyou-blessing">
           You will receive a confirmation email <span className="email"> {user.Email}</span>
-        </p>
+        </p> */}
 
         <div className="no-items-in-cart">
             <Link to="/brand" className="continue-shopping-button">
